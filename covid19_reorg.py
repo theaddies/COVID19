5587e5cba7367ff8b31042cf905a4e2629b0479e
Andrew Addie <theaddies@neo.rr.com>
2020-04-11 22:24:53 -0400

    contains output for both cases and deaths for USA

--- covid19_reorg.py
+++ covid19_reorg.py
@@ -1,23 +1,48 @@
 import pandas as pd
+#confirmed cases spreadsheet
 
-df = pd.read_csv (r'C:\Users\thead\Documents\COVID-19\csse_covid_19_data\csse_covid_19_time_series\time_series_covid19_confirmed_US.csv')   #read the csv file (put 'r' before the path string to address any special characters in the path, such as '\'). Don't forget to put the file name at the end of the path + ".csv"
-print (len(df))
-print(df.describe())
-print(df)
+COVID19_US_cases = pd.read_csv (r'C:\Users\thead\Documents\COVID-19\csse_covid_19_data\csse_covid_19_time_series\time_series_covid19_confirmed_US.csv')   #read the csv file (put 'r' before the path string to address any special characters in the path, such as '\'). Don't forget to put the file name at the end of the path + ".csv"
+#print (len(df))
+#print(df.describe())
+#print(df)
 
-df_new = df.drop(['UID','iso2','iso3','code3','FIPS','Admin2','Country_Region','Combined_Key','Lat','Long_'], axis=1)
+COVID19_US_cases_new = COVID19_US_cases.drop(['UID','iso2','iso3','code3','FIPS','Admin2','Country_Region','Combined_Key','Lat','Long_'], axis=1)
 
-print(df_new)
+print(COVID19_US_cases_new)
 
-df_new.to_csv(r'c:\Users\thead\Documents\COVID-19\csse_covid_19_data\csse_covid_19_time_series\df_new.csv',index=False)
+COVID19_US_cases_new.to_csv(r'c:\Users\thead\Documents\COVID-19\csse_covid_19_data\csse_covid_19_time_series\COVID19_US_cases_new.csv',index=False)
 
-df_new_sum = df_new.groupby(['Province_State']).sum()
+COVID19_US_cases_new_sum = COVID19_US_cases_new.groupby(['Province_State']).sum()
 
-df_new_sum.to_csv(r'c:\Users\thead\Documents\COVID-19\csse_covid_19_data\csse_covid_19_time_series\df_new_sum.csv')
+COVID19_US_cases_new_sum.to_csv(r'c:\Users\thead\Documents\COVID-19\csse_covid_19_data\csse_covid_19_time_series\COVID19_US_cases_new_sum.csv')
 
-df_new_transpose = df_new_sum.T
+COVID19_US_cases_new_transpose = COVID19_US_cases_new_sum.T
 
-print(df_new_transpose)
+COVID19_US_cases_new_transpose = COVID19_US_cases_new_transpose.drop(['American Samoa','Diamond Princess'], axis=1)
 
-df_new_transpose.to_csv(r'c:\Users\thead\Documents\COVID-19\csse_covid_19_data\csse_covid_19_time_series\df_out_transpose.csv')
+print(COVID19_US_cases_new_transpose)
 
+COVID19_US_cases_new_transpose.to_csv(r'c:\Users\thead\Documents\COVID-19\csse_covid_19_data\csse_covid_19_time_series\COVID19_US_cases_new_transpose.csv')
+
+#move on to the deaths spreadsheet
+
+
+COVID19_US_deaths = pd.read_csv (r'C:\Users\thead\Documents\COVID-19\csse_covid_19_data\csse_covid_19_time_series\time_series_covid19_deaths_US.csv')
+
+COVID19_US_deaths_new = COVID19_US_deaths.drop(['Population','UID','iso2','iso3','code3','FIPS','Admin2','Country_Region','Combined_Key','Lat','Long_'], axis=1)
+
+print(COVID19_US_deaths_new)
+
+COVID19_US_deaths_new.to_csv(r'c:\Users\thead\Documents\COVID-19\csse_covid_19_data\csse_covid_19_time_series\COVID19_US_deaths_new.csv',index=False)
+
+COVID19_US_deaths_new_sum = COVID19_US_deaths_new.groupby(['Province_State']).sum()
+
+COVID19_US_deaths_new_sum.to_csv(r'c:\Users\thead\Documents\COVID-19\csse_covid_19_data\csse_covid_19_time_series\COVID19_US_deaths_new_sum.csv')
+
+COVID19_US_deaths_new_transpose = COVID19_US_deaths_new_sum.T
+
+COVID19_US_deaths_new_transpose = COVID19_US_deaths_new_transpose.drop(['American Samoa','Diamond Princess'], axis=1)
+
+print(COVID19_US_deaths_new_transpose)
+
+COVID19_US_deaths_new_transpose.to_csv(r'c:\Users\thead\Documents\COVID-19\csse_covid_19_data\csse_covid_19_time_series\COVID19_US_deaths_new_transpose.csv')
